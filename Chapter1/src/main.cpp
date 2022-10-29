058ec75167e9ccd1d059a83677cb1abb6e6c8dff
KellSkog <kell.skog@gmail.com>
2022-10-29 17:54:24 +0200

    Environment set up

---
+++ Chapter1/src/main.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+
+int main() { std::cout << "I'm alive, alive I say!\n"; }
---
+++ Chapter1/test/main.cpp
@@ -0,0 +1,6 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+TEST_CASE("testing exercise 2") {
+  SUBCASE("2022-10-16") { CHECK(true); }
+}
